99eac3bef7163c3a3ddc863f94b262c9bc6a4620
Gabriel Torres <127896538+Gabe-Torres@users.noreply.github.com>
2024-03-01 20:15:12 -0600

    chore: clean up a bit

--- spec/features/nonplayer_characters/user_creates_a_new_nonplayer_character_spec.rb
+++ spec/features/nonplayer_characters/user_creates_a_new_nonplayer_character_spec.rb
@@ -1,29 +1,31 @@
 require 'rails_helper'
 
-RSpec.describe 'User creates a new nonplayer character' do
+RSpec.describe 'User creates a new nonplayer character', type: :feature do
   before(:each) do
     @user = User.create!(email: 'test@example.com', password: 'f4k3p455w0rd')
     login_as(@user, scope: :user)
     @campaign1 = @user.campaigns.create!(name: 'name 1', description: 'description 1')
   end
 
-  scenario 'a user creates a nonplayer character' do
-    visit campaign_path(@campaign1)
+  scenario 'a user creates a campaign' do
+    context 'within a campaign a user creates a new nonplayer character' do
+      visit campaign_path(@campaign1)
 
-    click_link 'Create New Nonplayer Character'
+      click_link 'Create New Nonplayer Character'
 
-    expect(current_path).to eq(new_campaign_nonplayer_character_path(@campaign1))
+      expect(current_path).to eq(new_campaign_nonplayer_character_path(@campaign1))
 
-    fill_in 'Name', with: 'Snoop Doggg'
-    fill_in 'Background', with: 'Hes basically a bard, but with a lot of weed.'
+      fill_in 'Name', with: 'Snoop Doggg'
+      fill_in 'Background', with: 'Hes basically a bard, but with a lot of weed.'
 
-    click_button 'Create new Nonplayer Character'
+      click_button 'Create new Nonplayer Character'
 
-    expect(current_path).to eq(campaign_path(@campaign1))
-    expect(page).to have_content('Nonplayer Character was successfully created.')
+      expect(current_path).to eq(campaign_path(@campaign1))
+      expect(page).to have_content('Nonplayer Character was successfully created.')
 
-    npc = NonplayerCharacter.find_by(name: 'Snoop Doggg')
-    expect(npc.background).to eq('Hes basically a bard, but with a lot of weed.')
-    expect(page).to have_content('Snoop Doggg')
+      npc = NonplayerCharacter.find_by(name: 'Snoop Doggg')
+      expect(npc.background).to eq('Hes basically a bard, but with a lot of weed.')
+      expect(page).to have_content('Snoop Doggg')
+    end
   end
 end
