df5989133fec360d9c182b0d75f15335a6d71de8
Gabriel Torres <127896538+Gabe-Torres@users.noreply.github.com>
2024-03-01 19:49:43 -0600

    add: relationship between campaign and npc

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,4 +1,6 @@
 class Campaign < ApplicationRecord
+    has_many :nonplayer_characters
+
     belongs_to :user
     validates_presence_of :name, :description
 end
--- spec/models/campaign_spec.rb
+++ spec/models/campaign_spec.rb
@@ -5,4 +5,9 @@ describe Campaign, type: :model do
     it {should validate_presence_of(:name)}
     it {should validate_presence_of(:description)}
   end
+  
+  describe "relationships" do
+    it {should belong_to(:user)}
+    it {should have_many(:nonplayer_characters)}
+  end
 end
