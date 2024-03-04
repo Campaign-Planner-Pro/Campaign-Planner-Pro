42f2888ddaedeca4326f27ee209b254f1450c304
Gabriel Torres <127896538+Gabe-Torres@users.noreply.github.com>
2024-03-03 19:37:30 -0600

    fix: delete for campaigns

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,5 +1,5 @@
 class Campaign < ApplicationRecord
-    has_many :nonplayer_characters
+    has_many :nonplayer_characters, dependent: :destroy
 
     belongs_to :user
     validates_presence_of :name, :description
