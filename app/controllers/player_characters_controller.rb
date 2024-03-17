dbe764c2570b716df0d42f87fb7cf429cc244fc7
Gabriel Torres <127896538+Gabe-Torres@users.noreply.github.com>
2024-03-17 14:04:11 -0500

    feature: user can create a player character

---
+++ app/controllers/player_characters_controller.rb
@@ -0,0 +1,29 @@
+class PlayerCharactersController < ApplicationController
+  before_action :set_campaign, only: [:new, :create]
+
+  def new
+    @campaign = Campaign.find(params[:campaign_id])
+    @player_character = @campaign.player_characters.new
+  end
+
+  def create
+    @player_character = @campaign.player_characters.new(player_character_params)
+
+    if @player_character.save
+      redirect_to campaign_path(@player_character.campaign_id)
+      flash[:notice] = 'Player Character was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def player_character_params
+    params.require(:player_character).permit(:name, :background)
+  end
+
+  def set_campaign
+    @campaign = Campaign.find(params[:campaign_id])
+  end
+end
